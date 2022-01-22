28bdb2c273717465dc0c5591503b40aad88073f6
mcelhinneyk <mr.mcelhinney@gmail.com>
2022-01-21 19:24:07 -0800

    commit tree d690a8c161735eecd29d3e7021ccfaec011dbe95

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (dwarf_names)
+  dwarf_names.each.with_index {|name, index| puts"#{index+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|planeteer| "#{planeteer.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.size > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(possible_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese_types.find {|cheese| possible_cheese.include?(cheese)?cheese:nil}
+      
+
 end
